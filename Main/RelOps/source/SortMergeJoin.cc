8b3850746a1430ba2f355503d7cc7e488da657f7
cj <ricezcj@gmail.com>
2017-04-08 23:35:17 -0500

    fix bug

--- Main/RelOps/source/SortMergeJoin.cc
+++ Main/RelOps/source/SortMergeJoin.cc
@@ -30,91 +30,91 @@ SortMergeJoin ::SortMergeJoin(MyDB_TableReaderWriterPtr leftInputIn, MyDB_TableR
 }
 
 
-void appendRecord (MyDB_PageReaderWriter &curPage, vector <MyDB_PageReaderWriter> &returnVal,
-                   MyDB_RecordPtr appendMe, MyDB_BufferManagerPtr parent) {
+//void appendRecord (MyDB_PageReaderWriter &curPage, vector <MyDB_PageReaderWriter> &returnVal,
+//                   MyDB_RecordPtr appendMe, MyDB_BufferManagerPtr parent) {
+//
+//    // try to append to the current page
+//    if (!curPage.append (appendMe)) {
+//
+//        // if we cannot, then add a new one to the output vector
+//        returnVal.push_back (curPage);
+//        MyDB_PageReaderWriter temp (*parent);
+//        temp.append (appendMe);
+//        curPage = temp;
+//    }
+//}
 
-    // try to append to the current page
-    if (!curPage.append (appendMe)) {
-
-        // if we cannot, then add a new one to the output vector
-        returnVal.push_back (curPage);
-        MyDB_PageReaderWriter temp (*parent);
-        temp.append (appendMe);
-        curPage = temp;
-    }
-}
-
-vector <MyDB_PageReaderWriter> mergeIntoList (MyDB_BufferManagerPtr parent, MyDB_RecordIteratorAltPtr leftIter,
-                                              MyDB_RecordIteratorAltPtr rightIter, function <bool ()> comparator, MyDB_RecordPtr lhs, MyDB_RecordPtr rhs) {
-
-    vector <MyDB_PageReaderWriter> returnVal;
-    MyDB_PageReaderWriter curPage (*parent);
-    bool lhsLoaded = false, rhsLoaded = false;
-
-    // if one of the runs is empty, get outta here
-    if (!leftIter->advance ()) {
-        while (rightIter->advance ()) {
-            rightIter->getCurrent (rhs);
-            appendRecord (curPage, returnVal, rhs, parent);
-        }
-    } else if (!rightIter->advance ()) {
-        while (leftIter->advance ()) {
-            leftIter->getCurrent (lhs);
-            appendRecord (curPage, returnVal, lhs, parent);
-        }
-    } else {
-        while (true) {
-
-            // get the two records
-
-            // here's a bit of an optimization... if one of the records is loaded, don't re-load
-            if (!lhsLoaded) {
-                leftIter->getCurrent (lhs);
-                lhsLoaded = true;
-            }
-
-            if (!rhsLoaded) {
-                rightIter->getCurrent (rhs);
-                rhsLoaded = true;
-            }
-
-            // see if the lhs is less
-            if (comparator ()) {
-                appendRecord (curPage, returnVal, lhs, parent);
-                lhsLoaded = false;
-
-                // deal with the case where we have to append all of the right records to the output
-                if (!leftIter->advance ()) {
-                    appendRecord (curPage, returnVal, rhs, parent);
-                    while (rightIter->advance ()) {
-                        rightIter->getCurrent (rhs);
-                        appendRecord (curPage, returnVal, rhs, parent);
-                    }
-                    break;
-                }
-            } else {
-                appendRecord (curPage, returnVal, rhs, parent);
-                rhsLoaded = false;
-
-                // deal with the ase where we have to append all of the right records to the output
-                if (!rightIter->advance ()) {
-                    appendRecord (curPage, returnVal, lhs, parent);
-                    while (leftIter->advance ()) {
-                        leftIter->getCurrent (lhs);
-                        appendRecord (curPage, returnVal, lhs, parent);
-                    }
-                    break;
-                }
-            }
-        }
-    }
-
-    // remember the current page
-    returnVal.push_back (curPage);
-
-    // outta here!
-    return returnVal;
-}
+//vector <MyDB_PageReaderWriter> mergeIntoList (MyDB_BufferManagerPtr parent, MyDB_RecordIteratorAltPtr leftIter,
+//                                              MyDB_RecordIteratorAltPtr rightIter, function <bool ()> comparator, MyDB_RecordPtr lhs, MyDB_RecordPtr rhs) {
+//
+//    vector <MyDB_PageReaderWriter> returnVal;
+//    MyDB_PageReaderWriter curPage (*parent);
+//    bool lhsLoaded = false, rhsLoaded = false;
+//
+//    // if one of the runs is empty, get outta here
+//    if (!leftIter->advance ()) {
+//        while (rightIter->advance ()) {
+//            rightIter->getCurrent (rhs);
+//            appendRecord (curPage, returnVal, rhs, parent);
+//        }
+//    } else if (!rightIter->advance ()) {
+//        while (leftIter->advance ()) {
+//            leftIter->getCurrent (lhs);
+//            appendRecord (curPage, returnVal, lhs, parent);
+//        }
+//    } else {
+//        while (true) {
+//
+//            // get the two records
+//
+//            // here's a bit of an optimization... if one of the records is loaded, don't re-load
+//            if (!lhsLoaded) {
+//                leftIter->getCurrent (lhs);
+//                lhsLoaded = true;
+//            }
+//
+//            if (!rhsLoaded) {
+//                rightIter->getCurrent (rhs);
+//                rhsLoaded = true;
+//            }
+//
+//            // see if the lhs is less
+//            if (comparator ()) {
+//                appendRecord (curPage, returnVal, lhs, parent);
+//                lhsLoaded = false;
+//
+//                // deal with the case where we have to append all of the right records to the output
+//                if (!leftIter->advance ()) {
+//                    appendRecord (curPage, returnVal, rhs, parent);
+//                    while (rightIter->advance ()) {
+//                        rightIter->getCurrent (rhs);
+//                        appendRecord (curPage, returnVal, rhs, parent);
+//                    }
+//                    break;
+//                }
+//            } else {
+//                appendRecord (curPage, returnVal, rhs, parent);
+//                rhsLoaded = false;
+//
+//                // deal with the ase where we have to append all of the right records to the output
+//                if (!rightIter->advance ()) {
+//                    appendRecord (curPage, returnVal, lhs, parent);
+//                    while (leftIter->advance ()) {
+//                        leftIter->getCurrent (lhs);
+//                        appendRecord (curPage, returnVal, lhs, parent);
+//                    }
+//                    break;
+//                }
+//            }
+//        }
+//    }
+//
+//    // remember the current page
+//    returnVal.push_back (curPage);
+//
+//    // outta here!
+//    return returnVal;
+//}
 
 void loaddata(MyDB_TableReaderWriterPtr cpfrom, MyDB_TableReaderWriterPtr cpto, string SelectionPredicate){
 
