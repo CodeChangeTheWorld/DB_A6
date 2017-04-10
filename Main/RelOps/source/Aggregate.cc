6141b299b5a8b7749ba71b7a3a52c58324f92f7a
liufang <liufangzka@hotmail.com>
2017-04-09 22:11:19 -0500

    debugging

--- Main/RelOps/source/Aggregate.cc
+++ Main/RelOps/source/Aggregate.cc
@@ -49,22 +49,29 @@ void Aggregate::run() {
     for (int i = 0; i < inputTable->getNumPages(); i++) {
         MyDB_PageReaderWriter temp = inputTable->getPinned (i);
         pagesize = temp.getPageSize();
-        if (temp.getType () == MyDB_PageType :: RegularPage)
+        if (temp.getType () == MyDB_PageType :: RegularPage){
             allData.push_back (inputTable->getPinned (i));
+//            allData.push_back();
+        }
+
     }
 
     cout<<"allData loaded"<<endl;
 
     //Scan Input table Write, write record to new page & Hash record
     MyDB_RecordPtr combinedRec = make_shared <MyDB_Record> (mySchemaOut);
+//    MyDB_RecordPtr combinedRec = outputTable->getEmptyRecord();
     MyDB_RecordIteratorAltPtr myIter = getIteratorAlt(allData);
 
     //MyDB_BufferManager (size_t pageSize, size_t numPages, string tempFile);
     MyDB_BufferManager bm = MyDB_BufferManager(pagesize,numpage, "Aggregate");
-    MyDB_PageReaderWriterPtr pageRW = make_shared <MyDB_PageReaderWriter>(bm);
+    MyDB_PageReaderWriterPtr pageRW = make_shared <MyDB_PageReaderWriter>(outputTable->getBufferMgr());
+
+    func finalPredicate = combinedRec->compileComputation (selectionPredicate);
 
     while (myIter->advance ()) {
         // hash the current record
+        cout<<"1234142"<<endl;
         myIter->getCurrent (inputRec);
         size_t hashVal = 0;
         int i=0;
@@ -78,7 +85,7 @@ void Aggregate::run() {
             combinedRec->getAtt(i++)->set(f());
         }
 
-        func finalPredicate = combinedRec->compileComputation (selectionPredicate);
+
         if(finalPredicate()->toBool()){
             void * ptr = pageRW->appendAndReturnLocation(combinedRec);
             myHash [hashVal].push_back (ptr);
