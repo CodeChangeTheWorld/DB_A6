a92060dbe45b56383df989ef959e5e5a52e2669b
liufang <liufangzka@hotmail.com>
2017-04-12 18:35:56 -0500

    fix bug

--- Main/RelOps/source/Aggregate.cc
+++ Main/RelOps/source/Aggregate.cc
@@ -133,14 +133,14 @@ void Aggregate::run() {
     }
 
     for ( auto it:myHash){
-        cout<<"Hash val new:"<< it.first<<endl;
+       // cout<<"Hash val new:"<< it.first<<endl;
         vector <void*> &groupRec = it.second;
         int count = groupRec.size();
-        cout<<"bucket count:"<<count;
+        //cout<<"bucket count:"<<count;
         for(int i=0;i<count;i++){
-            cout<<"groupRec[i]:"<<groupRec[i]<<endl;
+        //    cout<<"groupRec[i]:"<<groupRec[i]<<endl;
             tempRec->fromBinary(groupRec[i]);
-            cout<<"tempRec:"<<tempRec->getAtt(0).get()->toString() <<endl;
+         //   cout<<"tempRec:"<<tempRec->getAtt(0).get()->toString() <<endl;
 
             int app = -1;
             for(int j= groupNum; j<groupNum+aggNum;j++){
