1a71d256fd43a5e8bffc017ebf0bc966aaab32cd
liufang <liufangzka@hotmail.com>
2017-04-12 06:17:38 -0500

    debugging

--- Main/RelOps/source/Aggregate.cc
+++ Main/RelOps/source/Aggregate.cc
@@ -125,8 +125,8 @@ void Aggregate::run() {
     for (int i=0;i<aggsToCompute.size();i++) {
         auto s = aggsToCompute[i];
         if(s.first == MyDB_AggType::avg || s.first == MyDB_AggType::sum){
-            cout<<"Build Agg List: "<<"+(" + mySchemaOut->getAtts()[i+groupNum].first + ", [MyDB_AggAtt" + to_string (i) + "])" <<endl;
-            aggList.push_back(tempRec->compileComputation("+(" + mySchemaOut->getAtts()[i+groupNum].first + ", [MyDB_AggAtt" + to_string (i) + "])"));
+            cout<<"Build Agg List: "<<"+([" + mySchemaOut->getAtts()[i+groupNum].first + "], [MyDB_AggAtt" + to_string (i) + "])" <<endl;
+            aggList.push_back(tempRec->compileComputation("+([" + mySchemaOut->getAtts()[i+groupNum].first + "], [MyDB_AggAtt" + to_string (i) + "])"));
         }
 //        if(s.first == MyDB_AggType::avg){
 //            cout<<"Build Avg List: "<<"/( [MyDB_AggAtt" + to_string (i) + "],[MyCount])" <<endl;
